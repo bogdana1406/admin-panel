f2b432065a17d1d800cdf123ab1f9c2de4f6204c
bogdana1406 <bogdana1406@gmail.com>
2018-10-04 18:43:19 +0300

    first part

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+
+class AdminController extends Controller
+{
+    public function login(Request $request)
+    {
+        if($request->isMethod('post')){
+        $data = $request->input();
+        if(Auth::attempt(['email'=>$data['email'], 'password'=>$data['password'], 'admin'=>'1'])){
+            //echo "Success"; die;
+            return redirect('/admin/dashboard');
+        }else{
+            //echo "Failed"; die;
+            return redirect('/admin')->with('flash_massage_error', 'Invalid Username or Password');
+            }
+        }
+        return view('admin.admin_login');
+    }
+
+    public function dashboard()
+    {
+     return view('admin.dashboard');
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        return redirect('/admin')->with('flash_massage_success', 'Logged out Successfully');
+    }
+
+}
