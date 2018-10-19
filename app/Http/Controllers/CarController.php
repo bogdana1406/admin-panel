386a998ec8ff8ba1d9acef1a840b0406f11bddac
bogdana1406 <bogdana1406@gmail.com>
2018-10-19 18:28:17 +0300

    imige2

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -9,6 +9,9 @@ use Session;
 use App\Brand;
 use App\Engine;
 use App\Car;
+use Illuminate\Support\Facades\Input;
+use Image;
+
 
 class CarController extends Controller
 {
@@ -36,7 +39,26 @@ class CarController extends Controller
 //        $car->about = $data['about'];
 //        $car->description = $data['description'];
 //        $car->save();
+
+
           $car = Car::create($data);
+
+        if($request->hasFile('image')){
+            $image_tmp = Input::file('image');
+            if($image_tmp->isValid()){
+                $extension = $image_tmp->getClientOriginalExtension();
+                $filename = rand(111, 99999).".".$extension;
+                $large_image_path = 'images/backend_images/cars/large/'.$filename;
+                $medium_image_path = 'images/backend_images/cars/medium/'.$filename;
+                $small_image_path = 'images/backend_images/cars/small/'.$filename;
+
+                Image::make($image_tmp)->save($large_image_path);
+                Image::make($image_tmp)->resize(600,600)->save($medium_image_path);
+                Image::make($image_tmp)->resize(300,300)->save($small_image_path);
+                $car->image = $filename;
+            }
+        }
+
             if($car){
                 return redirect()->back()->with('flash_massage_success', 'Car has been added successfully');
             }else{
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -43,13 +43,13 @@ class ProductsController extends Controller
                if($image_tmp->isValid()){
                    $extension = $image_tmp->getClientOriginalExtension();
                    $filename = rand(111, 99999).".".$extension;
-                   $large_image_paht = 'images/backend_images/products/large/'.$filename;
-                   $medium_image_paht = 'images/backend_images/products/medium/'.$filename;
-                   $small_image_paht = 'images/backend_images/products/small/'.$filename;
+                   $large_image_path = 'images/backend_images/products/large/'.$filename;
+                   $medium_image_path = 'images/backend_images/products/medium/'.$filename;
+                   $small_image_path = 'images/backend_images/products/small/'.$filename;
 
-                   Image::make($image_tmp)->save($large_image_paht);
-                   Image::make($image_tmp)->resize(600,600)->save($medium_image_paht);
-                   Image::make($image_tmp)->resize(300,300)->save($small_image_paht);
+                   Image::make($image_tmp)->save($large_image_path);
+                   Image::make($image_tmp)->resize(600,600)->save($medium_image_path);
+                   Image::make($image_tmp)->resize(300,300)->save($small_image_path);
                    $product->image = $filename;
                }
             }
